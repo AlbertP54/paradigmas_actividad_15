3ba2ea0b27961212127a7197af00232bb7d2f3a3
Alberto Payan <a311047@uach.mx>
2021-05-06 20:22:23 -0700

    Actividad 15

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-const comprarCasco = () => {
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            resolve("Comprar casco")
-        },1000)
-    })
-}
+const lista = document.getElementById("lista");
+const url = "https://jsonplaceholder.typicode.com/photos";
 
-const comprarGuantes = () => {
-    return new Promise((resolve, reject)=>{
-        setTimeout(()=>{
-            resolve("Comprar guantes")
-        },2000)
+const getPosts = async () =>{
+     const Response = await fetch(url);
+     const Posts = await Response.json();
+     Posts.map(elemento =>{
+        var li = document.createElement('li');
+        li.innerHTML =`<a href ="detalle.html?id=${elemento.id}">${elemento.id}</a> ${elemento.title}`
+        lista.appendChild(li);
     })
+    return Posts;
 }
+getPosts();
